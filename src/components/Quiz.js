f7dfb7182256604f8c6799955f9f5343e7508e10
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-23 02:05:40 +0800

    Added mathjax on index.html

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
 import { results } from '../data/results'
+import { M } from '../Math'
 
 const QuizCategories = () => {
 	const [categories, setCategories] = useState([])
 	const fetchQuizCategories = () => {
-		
 		const formattedData = results.map(category => {
 			const incorrectAnswersIndexes = category.incorrect_answers.length
 			const randomIndex = Math.random() * (incorrectAnswersIndexes - 0) + 0
@@ -22,7 +22,13 @@ const QuizCategories = () => {
 	}, [])
 
 	console.log({ categories })
-	return <div>Quiz Categories</div>
+	return (
+		<div>
+			What is the value of 
+			<M m='\pi' />
+      {results[0].question}
+		</div>
+	)
 }
 
 export default QuizCategories
